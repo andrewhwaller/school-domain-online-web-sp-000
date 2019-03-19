d43fd1fef07045c3964e857d49ee10b1187b74f2
Andrew Waller <andrewhwaller@gmail.com>
2019-03-19 09:16:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,7 +6,7 @@ class School
     @roster = {}
   end
 
-  def add_student(student_grade, student_name)
+  def add_student(student_name, student_grade)
     roster[student_grade] ||= []
     roster[student_grade] << "student_name"
   end
