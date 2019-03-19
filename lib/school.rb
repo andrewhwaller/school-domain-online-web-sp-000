d55947243362055e3cd36452fee28c024642648d
Andrew Waller <andrewhwaller@gmail.com>
2019-03-19 09:12:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,9 +5,9 @@ class School
     @roster = {}
   end
 
-  def add_student(student_name, student_grade)
-    @roster[student_name] = []
-    @roster[student_name] << "student_grade"
+  def add_student(student_grade, student_name)
+    @roster[student_grade] = []
+    @roster[student_grade] << "student_name"
   end
 
   def grade(student_grade)
