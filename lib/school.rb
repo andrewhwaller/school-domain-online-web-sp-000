12863e0375ae81a36c8cf4fd4f8af26b30950425
Andrew Waller <andrewhwaller@gmail.com>
2019-03-19 09:05:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,13 +2,13 @@ class School
 
   def initialize(name)
     @name = name
-    self.roster = {}
+    @roster = {}
   end
 
   def add_student(student, grade_level)
   end
 
   def grade(student, grade_level)
-   @roster.key(grade_level)
+   roster.key(grade_level)
   end
 end
